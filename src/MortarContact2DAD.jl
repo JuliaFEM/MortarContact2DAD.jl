d00130ffd6353bd06e4cfc42e5a84ed40a812b0b
Jukka Aho <ahojukka5@gmail.com>
2018-05-15 14:59:46 +0300

    Do not export functions
    - Same than in MortarContact2D.jl.

--- src/MortarContact2DAD.jl
+++ src/MortarContact2DAD.jl
@@ -2,7 +2,7 @@
 # License is MIT: see https://github.com/JuliaFEM/MortarContact2DAD.jl/blob/master/LICENSE
 
 """
-    2D mortar contact mechanics for JuliaFEM using Automatic Differentiation
+2D mortar contact mechanics for JuliaFEM using Automatic Differentiation
 """
 module MortarContact2DAD
 
@@ -10,15 +10,11 @@ using ForwardDiff
 
 using Reexport
 @reexport using FEMBase
-import FEMBase: get_unknown_field_name, assemble_elements!, add_elements!
 
 const MortarElements2D = Union{Seg2,Seg3}
 
 include("mortar2dad.jl")
 include("contact2dad.jl")
 export Mortar2DAD, Contact2DAD
-export add_slave_elements!, add_master_elements!
-export get_slave_elements, get_master_elements
-export get_slave_dofs, get_master_dofs
 
 end
